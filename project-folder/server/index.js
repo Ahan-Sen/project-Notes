7b46de4e8e5af2a97eb49855330e15cc3f417626
Ahan Sen <senahan003@gmail.com>
2021-03-05 16:51:46 +0530

    setting up

---
+++ project-folder/server/index.js
@@ -0,0 +1,16 @@
+let express = require("express");
+
+const app = express();
+
+const connectDB = require("./config/db");
+//connect to database
+connectDB();
+
+app.use(express.json({ extended: true }));
+
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/notes", require("./routes/notes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
